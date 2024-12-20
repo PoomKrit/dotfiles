d2bb624b1edd79fd04c9f47edbb5716a1a480cca
KKULJITTISUTEEPORN <krittanon.kuljittisuteeporn@accor.com>
2024-12-20 14:11:24 +0700

    Remove opt block
    Avoid duplication

--- .config/nvim/lua/josean/plugins/noise.lua
+++ .config/nvim/lua/josean/plugins/noise.lua
@@ -12,11 +12,11 @@ return {
 		--   If not available, we use `mini` as the fallback
 		{
 			"rcarriga/nvim-notify",
-			opts = {
-				render = "wrapped-compact", -- Choose between "default", "minimal", "simple", or "compact"
-				timeout = 5000, -- Adjust timeout duration (5000ms = 5 seconds)
-				sticky = true, -- Keeps notifications visible until dismissed
-			},
+			-- opts = {
+			-- 	render = "wrapped-compact", -- Choose between "default", "minimal", "simple", or "compact"
+			-- 	timeout = 5000, -- Adjust timeout duration (5000ms = 5 seconds)
+			-- 	sticky = true, -- Keeps notifications visible until dismissed
+			-- },
 			config = function()
 				local notify = require("notify")
 				notify.setup({
