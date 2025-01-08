edf174630930df74405bb7421c293fe3a667117f
CTA.KKULJITTISUTEEPORN@accor.onmicrosoft.com <cta.kkuljittisuteeporn@accor.onmicrosoft.com>
2025-01-08 17:21:29 +0700

    Making conceallevel = 2 for Obsidian neovim features

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,4 @@ vim.opt.mouse = "a"
 -- Invert horizontal scrolling
 vim.api.nvim_set_keymap("n", "<ScrollWheelLeft>", "z5l", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<ScrollWheelRight>", "z5h", { noremap = true, silent = true })
+vim.opt.conceallevel = 2
