3ab02f2d62d02e49a4499e66645141971fef04b1
PoomKrit <my.mystrymail@gmail.com>
2025-01-10 17:13:36 +0700

    adding comment

--- .config/nvim/lua/josean/plugins/auto-session.lua
+++ .config/nvim/lua/josean/plugins/auto-session.lua
@@ -1,12 +1,16 @@
+-- provides automatic session management.
+-- It helps you save and restore your workspace,
+-- including open buffers, windows, and tab pages,
+-- so you can easily continue where you left off in a project.
+
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
 
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
-    })
-
-  end,
+		auto_session.setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+		})
+	end,
 }
