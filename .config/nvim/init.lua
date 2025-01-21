022de236cd5e48f0e6ba7f137191bf12a54a6d26
KKULJITTISUTEE <krittanon.kuljittisuteeporn@accor.com>
2025-01-21 20:05:24 +0700

    adding line for escaping from terminal mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,4 +7,5 @@ vim.opt.mouse = "a"
 -- Invert horizontal scrolling
 vim.api.nvim_set_keymap("n", "<ScrollWheelLeft>", "z5l", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<ScrollWheelRight>", "z5h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-x>", [[<C-\><C-n>]], { noremap = true, silent = true })
 vim.opt.conceallevel = 2
