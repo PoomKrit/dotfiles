b1b4e923d4c855d5b16f5853162b638a8def52cd
KKULJITTISUTEE <krittanon.kuljittisuteeporn@accor.com>
2025-02-03 10:28:36 +0700

    use the new config

--- .config/nvim/lua/josean/plugins/render-markdown.lua
+++ .config/nvim/lua/josean/plugins/render-markdown.lua
@@ -1,9 +1,37 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'echasnovski/mini.nvim',
+    },
     opts = {},
+    config = function()
+        local hl = vim.api.nvim_set_hl
+
+        -- Define custom highlight for code blocks
+        hl(0, "MarkdownCodeBlockNormal", { bg = "#44475a", fg = "#f8f8f2", bold = true })
+
+        -- Link Treesitter groups
+        hl(0, "@markup.raw.block.markdown", { link = "MarkdownCodeBlockNormal" })
+        hl(0, "@text.literal.markdown", { link = "MarkdownCodeBlockNormal" })
+
+        -- Enable Treesitter highlighting
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { "markdown", "markdown_inline" },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "markdown" },
+            },
+        })
+
+        -- Autocommands for persistent highlights
+        vim.api.nvim_exec([[
+            augroup MarkdownHighlightFix
+                autocmd!
+                autocmd FileType markdown highlight MarkdownCodeBlockNormal guibg=#44475a guifg=#f8f8f2 gui=bold
+                autocmd FileType markdown highlight @markup.raw.block.markdown guibg=#44475a guifg=#f8f8f2 gui=bold
+                autocmd FileType markdown highlight @text.literal.markdown guibg=#44475a guifg=#f8f8f2 gui=bold
+            augroup END
+        ]], false)
+    end,
 }
