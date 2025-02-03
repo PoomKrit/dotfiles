215efd219159539f31d84294bd1e49bd83ccb0dd
KKULJITTISUTEE <krittanon.kuljittisuteeporn@accor.com>
2025-02-03 11:06:55 +0700

    Move lazy_status and lualine to be close to setup function

--- .config/nvim/lua/josean/plugins/lualine.lua
+++ .config/nvim/lua/josean/plugins/lualine.lua
@@ -2,8 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     local colors = {
       blue = "#65D1FF",
@@ -48,6 +46,9 @@ return {
         c = { bg = colors.inactive_bg, fg = colors.semilightgray },
       },
     }
+
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     -- configure lualine with modified theme
     lualine.setup({
