ab85c7e21148cb0174a670c0dbd6fdb86e3232d7
KKULJITTISUTEE <krittanon.kuljittisuteeporn@accor.com>
2025-04-07 19:18:41 +0700

    Codesnap to clipboard seems not to work

--- .config/nvim/lua/josean/plugins/codesnap.lua
+++ .config/nvim/lua/josean/plugins/codesnap.lua
@@ -2,8 +2,8 @@ return {
 	"mistricky/codesnap.nvim",
 	build = "make build_generator",
 	keys = {
-		{ "<leader>cc", "<cmd>'<,'>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-		{ "<leader>cs", "<cmd>'<,'>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+		-- { "<leader>cc", "<cmd>'<,'>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+		{ "<leader>cs", "<cmd>'<,'>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Downloads" },
 	},
 	opts = {
 		save_path = "~/Downloads",
